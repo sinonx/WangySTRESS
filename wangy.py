b116c28294fa3eccd99f0ac05b07653433d643e2
Abdi Pranata <39010800+sinonx@users.noreply.github.com>
2021-05-28 03:08:00 +0700

    Add files via upload

---
+++ wangy.py
@@ -0,0 +1,121 @@
+
+
+def wangy():
+	chara = input('Nama Waifu/Karakter mu? ')
+	print('\n')
+	print('###############################################################'+'\n')
+	wangee1 = chara + ' ' + chara + ' ' + chara +  ' WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya rambut ' + chara + ' wangi aku mau nyiumin aroma wanginya ' + chara + ' AAAAAAAAH ~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~ AAAAAH ' + '\n'
+	wangee2 = chara + ' keluar pertama kali di anime juga manis banget AAAAAAAAH ' + chara + ' AAAAA LUCCUUUUUUUUUUUUUUU............ ' + '\n'
+	wangee3 = chara + ' AAAAAAAAAAAAAAAAAAAAGH apa ? ' + chara + ' itu gak nyata ? Cuma HALU katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK PEDULI BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. ' + '\n'
+	wangee4 = chara + ' gw ... ' + chara + ' di laptop ngeliatin gw, ' + chara + ' .. kamu percaya sama aku ? aaaaaaaaaaah syukur ' + '\n'
+	wangee5 = chara + ' aku gak mau merelakan ' + chara + ' aaaaaah YEAAAAAAAAAAAH GUA MASIH PUNYA ' + chara + ' SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAAH' + '\n'
+	p = wangee1 + wangee2 + wangee3 + wangee4 + wangee5 
+	print(p)
+	asu = '{} wangyy.txt'.format(chara)
+	open(asu, 'a').write(p+'\n')
+	print('###############################################################')
+	print('Saved!')
+
+def gemeter():
+	chara = input('Nama Waifu/Karakter mu? ')
+	print('\n')
+	print('###############################################################'+'\n')
+	geme1 = 'Bro, gue gemeteran. GUE GEMETERAN ANJING Gue gak pernah mau berkembang biak dengan siapapun lebih daripada seorang ' + chara + '\n'
+	geme2 = 'Badannya yang cakep, TETE GEDE, pinggul seksi dari seorang BIDADARI. Jujur aja, sakit hati kalau tau KENYATAANNYA gue GAK AKAN PERNAH BISA BUAT KAWIN SAMA DIA, ngewarisin gen gue lewat dia, dan ngebiarin dia ngelahirin anak yang sempurna dari gue. ' + '\n'
+	geme3 =	'Gue rela ngelakuin APAPUN demi kesempatan buat bikin ' + chara + ' hamil. A P A P U N. Dan gue bener-bener gk bisa terima kenyataan. ' + '\n'
+	geme4 = 'Kenapa Authornya rela bikin suatu hal yang sempurna kyk dia? Buat ngegoda kita? NGETAWAIN KITA DI DEPAN MUKA? SUMPAH BRO, GUE UDAH BENER BENER GAK TAHAN. ANJING. ' + '\n'
+	p = geme1 + geme2 + geme3 + geme4
+	print(p)
+	asu = '{} gemeter.txt'.format(chara)
+	open(asu, 'a').write(p+'\n')
+	print('###############################################################')
+	print('Saved!')
+
+def simp():
+	chara = input('Nama Waifu/Karakter mu? ')
+	print('\n')
+	print('###############################################################'+'\n')
+	sem1 = 'Buruan, panggil gue SIMP, ato BAPERAN. ini MURNI PERASAAN GUE. Gue pengen genjot bareng ' + chara + ' . Ini seriusan, suaranya yang imut, mukanya yang cantik, apalagi badannya yang aduhai ningkatin gairah gue buat genjot ' + chara + ' .' + '\n' 
+	sem2 = 'Setiap lapisan kulitnya pengen gue jilat. Saat gue mau crot, gue bakal moncrot sepenuh hati, bisa di perut, muka, badan, teteknya, sampai lubang burit pun bakal gue crot sampai puncak klimaks. ' + '\n'
+	sem3 = 'Gue bakal meluk dia abis gue moncrot, lalu nanya gimana kabarnya, ngrasain enggas bareng saat telanjang. ' + '\n'
+	sem4 = 'Dia bakal bilang kalau genjotan gue mantep dan nyatain perasaannya ke gue, bilang kalo dia cinta ama gue. Gue bakal bilang balik seberapa gue cinta ama dia, dan dia bakal kecup gue di pipi. ' + '\n' 
+	sem5 = 'Terus kita ganti pakaian dan ngabisin waktu nonton film, sambil pelukan ama makan hidangan favorit. Gue mau ' + chara + ' jadi pacar, pasangan, istri, dan idup gue. Gue cinta dia dan ingin dia jadi bagian tubuh gue. ' + '\n'
+	sem6 = 'Lo kira ini copypasta? Kagak cok. Gue ngetik tiap kata nyatain prasaan gue. Setiap kali elo nanya dia siapa, denger ini baik-baik : DIA ISTRI GUE. ' + '\n'
+	sem7 = 'Gue sayang ' + chara + ' , dan INI MURNI PIKIRAN DAN PERASAAN GUE. ' + '\n'
+	p = sem1 + sem2 + sem3 + sem4 + sem5 + sem6 + sem7
+	print(p)
+	asu = '{} simp.txt'.format(chara)
+	open(asu, 'a').write(p+'\n')
+	print('###############################################################')
+	print('Saved!')
+
+def nenen():
+	chara = input('Nama Waifu/Karakter mu? ')
+	print('\n')
+	print('###############################################################'+'\n')
+	nen1 = 'NENEN NENEN KEPENGEN NENEN SAMA ' + chara + ' . TETEK GEDE NAN KENCANG MILIK ' + chara + ' MEMBUATKU KEPENGEN NENEN. DIBALUT PAKAIAN KETAT YANG ADUHAI CROOOOTOTOTOTOTOT ANJING SANGE GUA BANGSAT. ' + '\n'
+	nen2 = chara + ' , PLIS DENGERIN BAIK BAIK. TOLONG BUKA BAJU SEBENTAR SAJA PLISSS TOLOOONG BANGET, BIARKAN MULUT KERINGKU BISA MENGECAP NENEN ' + chara + ' . BIARKAN AKU MENGENYOT NENENMU ' + chara + ' .' + '\n'
+	nen3 = 'AKU RELA NGASIH SESEMBAHAN APA AJA BERAPAPUN ITU DUIT YANG AKU BAKAR KHUSUS TERKHUSUS BUATMU. ' + '\n'
+	nen4 = 'TAPI TOLOOOONG BANGET BUKA BAJUMU AKU MAU NENEN. NENEN NENEEEEN NENEN ' + chara + ' WANGIIII ' + '\n'
+	p = nen1 + nen2 + nen3 + nen4
+	print(p)
+	asu = '{} nenen.txt'.format(chara)
+	open(asu, 'a').write(p+'\n')
+	print('###############################################################')
+	print('Saved!')
+ 
+def klaim():
+	chara = input('Nama Waifu/Karakter mu? ')
+	print('\n')
+	print('###############################################################'+'\n')
+	klem1 = 'Sejujurnya gw ga nyangka ama tindakan lo yg ga dewasa begini Kita udh temenan dri kecil ,melalui berbagai kenangan ,tapi sikaplo begini ke gw ,ga habis pikir. ' + '\n'
+	klem2 = 'Padahal sudah berjanji tidak mengusik hubungan satu sama lain lagi ,tapi maksud tindakan mu sekarang ini apa? Tiba tiba di pagi bangun tidur lu make Pp ' + chara + ' .' + '\n' 
+	klem3 = 'Lu kira lucu begitu anjing? Make waifu pp org seenaknya? Ngeklaim pula bangsad maksudnya apa apaan coba. ' + '\n'
+	klem4 = 'Pertemanan dari kecil kita ga dihargai sama sekali. Gw tunggu klarifikasi lo ' + '\n'
+	p = klem1 + klem2 + klem3 + klem4
+	print(p)
+	asu = '{} klaim.txt'.format(chara)
+	open(asu, 'a').write(p+'\n')
+	print('###############################################################')
+	print('Saved!')
+
+def banner():
+
+	print("""
+
+   _____ _______ _____  ______  _____ _____ 
+  / ____|__   __|  __ \|  ____|/ ____/ ____|
+ | (___    | |  | |__) | |__  | (___| (___  
+  \___ \   | |  |  _  /|  __|  \___ \\___ \ 
+  ____) |  | |  | | \ \| |____ ____) |___) |
+ |_____/   |_|  |_|  \_\______|_____/_____/ 
+                                            
+      STRESS GENERATOR BUT CLI VERSION                           
+
+		"""+'\n') 
+
+#Coded by SinonX
+#Thanks for Firsta Adi Pradana
+
+	print('(+) 1. Wangyy STRESS')
+	print('(+) 2. Gemeteran STRESS')
+	print('(+) 3. SIMP STRESS')
+	print('(+) 4. Nenen STRESS')
+	print('(+) 5. Klaim Waifu STRESS'+'\n')
+
+	pilih = input('Pilih KeSTRESSan -> ')
+	if pilih == '1':
+		wangy()
+	elif pilih == '2':
+		gemeter()
+	elif pilih == '3':
+		simp()
+	elif pilih == '4':
+		nenen()
+	elif pilih == '5':
+		klaim()
+	else:
+		print('Pilihan tidak tersedia!')
+
+if __name__ == "__main__":
+	banner()
